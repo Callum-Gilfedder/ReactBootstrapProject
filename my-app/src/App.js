a447560d8158bdf3f166c6e70c9390220ed6e657
Callum Gilfedder <Callumgilf@mail.com>
2023-01-12 10:30:36 +0000

    Implementing React

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar.js' 
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
