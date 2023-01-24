e914310fccf2e74a069d79592b2d63bd9a7df622
Callum Gilfedder <Callumgilf@mail.com>
2023-01-24 11:04:41 +0000

    Major styles update

--- my-app/src/Carousel.js
+++ my-app/src/Carousel.js
@@ -1,16 +1,22 @@
 import CarouselImageBook from "./CarouselImageBook.png" 
 import img1 from "./img1.webp"
 import img2 from "./img2.webp"
+import CarouselImageBook2 from "./CarouselImageBook2.png"
+import CarouselImageBook3 from "./CarouselImageBook3.png"
+
 
 export default function Carousel() {
     return (
-        <div id="carouselExample" className="carousel slide">
+        <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-inner">
-            <div className="carousel-item text-center">
-            <img src={img2} className="d-block w-100 carousel-background-img" alt=""/>
+            <div className="carousel-item">
+            <div className="carousel-text-heading">'Conversations with Friends, Sally Rooney. '</div>
+              <div className="carousel-text-small"> - Person</div>
 
-              <div className="display-1" >SALE <br/> 50% OFF</div>
-              <button type="button" className="btn btn-outline-light">SHOP NOW</button>
+              <button type="button" className="btn btn-outline-light carousel-button">Order now</button>
+
+              <img src={img2} className="w-100 carousel-background-img" alt=""/>
+              <img src={CarouselImageBook2} className="d-block w-100 small-carousel-img d-inline" alt=""/>
             </div>
             <div className="carousel-item active">
               <div className="carousel-text-heading">'Darkly funny and told with unflinching candour'</div>
@@ -25,8 +31,13 @@ export default function Carousel() {
               <div className="display-1" ></div>
             </div>
             <div className="carousel-item">
-              <img src={img2} className="d-block w-100 carousel-background-img" alt=""/>
-            </div>
+            <div className="carousel-text-heading">'Darkly funny and told with unflinching candour'</div>
+              <div className="carousel-text-small"> - WaterStones</div>
+
+              <button type="button" className="btn btn-outline-light carousel-button">Order now</button>
+
+              <img src={img1} className="w-100 carousel-background-img" alt=""/>
+              <img src={CarouselImageBook3} className="d-block w-100 small-carousel-img d-inline" alt=""/>            </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
