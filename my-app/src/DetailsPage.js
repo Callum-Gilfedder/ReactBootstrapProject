df864c1666ccc6135a9f2ce0c1ec74c0c6ef2104
Callum Gilfedder <callumgilf@mail.com>
2023-02-27 11:17:17 +0000

    Added handleClickPlus to detailsPage

--- my-app/src/DetailsPage.js
+++ my-app/src/DetailsPage.js
@@ -1,4 +1,33 @@
+
+import { useContext, useState } from "react"
+import { MyContext } from './Contexts/Context'
+
+
 export default function DetailsPage(props) {
+    const { count1, setCount1 } = useContext(MyContext);
+    const { count2, setCount2 } = useContext(MyContext);
+    const { count3, setCount3 } = useContext(MyContext);
+    const { count4, setCount4 } = useContext(MyContext);
+    const { count5, setCount5 } = useContext(MyContext);
+    const { count6, setCount6 } = useContext(MyContext);
+    
+    function handleClickPlus() {
+        if (props.title == "1984") {
+            setCount1(count1 + 1)
+        } else if (props.title == "The Great Gatsby") {
+            setCount2(count2 + 1)
+        } else if (props.title == "The Little Prince") {
+            setCount3(count3 + 1)
+        } else if (props.title == "Im Glad My Mom Died") {
+            setCount4(count4 + 1)
+        } else if (props.title == "The Catcher In The Rye") {
+            setCount5(count5 + 1)
+        } else if (props.title == "A Clockwork Orange") {
+            setCount6(count6 + 1)
+        }
+    }
+
+
     return (
         <div className="container">
             <div class="container details-container" >
@@ -21,7 +50,7 @@ export default function DetailsPage(props) {
 
                         <button type="button" className="btn btn-dark btn-md details-btn" style={{marginRight: "1rem"}}>Checkout</button>
 
-                        <button type="button" className="btn btn-dark btn-md details-btn" onClick={() => {props.setCount(props.count + 1)}}>Add to Basket</button>
+                        <button type="button" className="btn btn-dark btn-md details-btn" onClick={handleClickPlus}>Add to Basket</button>
                     </div>
                 </div>
                 </div>
