99ce940c76bbd5970db23d87c148ed8c15d98b11
Callum Gilfedder <callumgilf@mail.com>
2023-02-28 11:26:07 +0000

    Added basket popup to explore category pages

--- my-app/src/ExploreCategory.js
+++ my-app/src/ExploreCategory.js
@@ -1,11 +1,21 @@
 import Row from "./Row.js"
+import { useState } from "react"
+import PopUp from "./PopUp.js";
 
 export default function ExploreCategory(props) {
+    const [popUpTriggered, setPopUpTriggered] = useState(false)
+    function deactivatePopUp() {
+        setPopUpTriggered(false);
+    }
+    setTimeout(deactivatePopUp, 5000)    
+
+
     return (
         <div>
-        <Row rowtitle={"Explore Our " + props.category + " Collection (These are sample books)"}></Row>
-        <Row rowtitle=""></Row>
-        <Row rowtitle=""></Row>
+        <Row rowtitle={"Explore Our " + props.category + " Collection (These are sample books)"} popUpTriggered={popUpTriggered} setPopUpTriggered={setPopUpTriggered}></Row>
+        <Row rowtitle="" popUpTriggered={popUpTriggered} setPopUpTriggered={setPopUpTriggered}></Row>
+        <Row rowtitle="" popUpTriggered={popUpTriggered} setPopUpTriggered={setPopUpTriggered}></Row>
+        {popUpTriggered ? <PopUp></PopUp>: null}
         </div>
     )
 }
