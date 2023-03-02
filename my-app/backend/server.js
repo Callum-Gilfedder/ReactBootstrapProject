3947e0b7d7616d29d419932d33ccae03f831d3b7
Callum Gilfedder <callumgilf@mail.com>
2023-03-02 12:14:13 +0000

    backend progress

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express")
-
+const cors = require("cors")
 const app = express()
 
-app.get("/test", (req, res) => {
-    res.send("testing")
+app.use(cors())
+app.use(express.json())
+
+app.post("/api/register", (req, res) => {
+    console.log(req.body)
+    res.json()
 })
 
 app.listen(3000, () => {
--- my-app/src/Register.js
+++ my-app/src/Register.js
@@ -8,6 +8,7 @@ export default function Contact () {
    async function registerUser (event) {
         event.preventDefault()
         const response = await fetch("http://localhost:3000/api/register", {
+            method: "POST",
             headers: {
                 "Content-type": "application/json",
 
